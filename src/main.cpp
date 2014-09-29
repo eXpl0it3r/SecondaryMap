0530389e9a5a726742e6de684382ac2db45bb8f4
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2014-09-29 14:47:30 +0200

    Added basic exception handling and logging.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include "Application.hpp"
 
+#include <SFML/System/Err.hpp>
+
+#include <sstream>
+#include <fstream>
+
 int main()
 {
-	Application{}.run();
+	// Redirect sf::err() for user-friendly error handling
+	std::stringstream errors;
+	sf::err().rdbuf(errors.rdbuf());
+
+	try
+	{
+		Application{}.run();
+	}
+	catch (std::exception& e)
+	{
+		sf::err() << "EXCEPTION: " << e.what() << std::endl;
+
+		std::ofstream log("SecondaryMap.log");
+		log << errors.str() << std::endl;
+
+		throw std::runtime_error("SecondaryMap has experienced an runtime error. Check the SecondaryMap.log and/or contact the developer.");
+	}
 }
